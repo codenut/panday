5008450dc674fc26d47a7a526a7b119f218d8dd5
Michael Valladolid <mikevalladolid@gmail.com>
2019-02-06 16:24:27 +0800

    devise installation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :people, path: 'auth', path_names: {
+    sign_in: 'login', sign_out: 'logout', registration: 'register',
+    confirmation: 'verification', unlock: 'unblock' }
 end
