b31b18eb4a0660b0cdb821924e0e3ad7d7929804
Michael Valladolid <mikevalladolid@gmail.com>
2019-02-06 20:48:34 +0800

    rubocop formatting

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,27 +1,28 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Person, type: :model do
   let(:person) { build(:person) }
 
-  describe "#create" do
-    context "valid person" do
-      it "should create a person in panday database" do
+  describe '#create' do
+    context 'valid person' do
+      it 'should create a person in panday database' do
         person.save
         expect(person).not_to be_nil
         expect(person).to be_valid
       end
 
-      it "should have at least phone number present" do
-        person = create(:person, username: Faker::Name.unique.name,
+      it 'should have at least phone number present' do
+        person = create(:person,
+                        username: Faker::Name.unique.name,
                         email: nil,
-                        phone_number: Faker::PhoneNumber::unique.phone_number)
+                        phone_number: Faker::PhoneNumber.unique.phone_number)
         expect(person).not_to be_nil
         expect(person).to be_valid
       end
     end
 
-    context "invalid person" do
-      it "should not create a person with no email or phone number" do
+    context 'invalid person' do
+      it 'should not create a person with no email or phone number' do
         person = build(:person, email: nil, phone_number: nil)
         expect(person).not_to be_nil
         expect(person).not_to be_valid
@@ -29,9 +30,9 @@ describe Person, type: :model do
     end
   end
 
-  describe "#update_attributes" do
-    context "valid person" do
-      it "should update person" do
+  describe '#update_attributes' do
+    context 'valid person' do
+      it 'should update person' do
         person.save
         person.update_attributes(first_name: 'Mike')
         expect(person.first_name).to eq('Mike')
